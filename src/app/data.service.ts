1c8f1d5c6534f8bc5195b0d93dd6d2cd701c1768
daniel trujillo <oscartrujillo159@gmail.com>
2024-11-11 15:33:01 -0600

    Configuracion de dataService

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,38 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { vehiculo } from './vehiculo.models';
-import { map, Observable } from 'rxjs';
+      import { Injectable } from '@angular/core';
+      import { HttpClient } from '@angular/common/http';
+      import { vehiculo } from './vehiculo.models';
+      import { map, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+      @Injectable({
+        providedIn: 'root',
+      })
 
-export class DataService {
-  private url = 'https://daniel-trujillo-ing-default-rtdb.firebaseio.com/navidad.json';
-  httpClient: any;
+      export class DataService {
+        private url = 'https://daniel-trujillo-ing-default-rtdb.firebaseio.com/navidad.json';
+        httpClient: any;
 
-  constructor(private http: HttpClient) {}
+        constructor(private http: HttpClient) {}
 
-  // Método para agregar un nuevo vehículo
-  agregarVehiculo(vehiculo: vehiculo): Observable<any> {
-    return this.http.post(this.url, vehiculo);
-  }
+        // metodo para agregar un nuevo registro
+        
+        agregarVehiculo(vehiculo: vehiculo): Observable<any> {
+          return this.http.post(this.url, vehiculo);
+        }
 
-  // Método para obtener todos los vehículos
-  obtenerVehiculos(): Observable<vehiculo[]> {  
-    return this.http.get<vehiculo[]>(this.url).pipe(  
-      map((data) => Object.values(data)) // Convierte el objeto en un arreglo  
-    );  
-  }
+        // metodo para obtener todos los vehiculo
 
-  // Método para actualizar un vehículo
-  actualizarVehiculo(id: string, vehiculo: vehiculo): Observable<any> {
-    return this.http.put(`${this.url}/${id}.json`, vehiculo);
-  }
+        obtenerVehiculos(): Observable<vehiculo[]> {  
+          return this.http.get<vehiculo[]>(this.url).pipe(  
+            map((data) => Object.values(data))
+          );  
+        }
 
-  // Método para eliminar un vehículo
-  eliminarVehiculo(id: string): Observable<any> {
-    return this.http.delete(`${this.url}/${id}.json`);
-  }
+        // metodo para actualizar un vehiculo
 
-}
+        actualizarVehiculo(id: string, vehiculo: vehiculo): Observable<any> {
+          return this.http.put(`${this.url}/${id}.json`, vehiculo);
+        }
+
+        // metodo para eliminar un vehiculo
+
+        eliminarVehiculo(id: string): Observable<any> {
+          return this.http.delete(`${this.url}/${id}.json`);
+        }
+
+      }
--- src/app/vehiculos.service.ts
+++ src/app/vehiculos.service.ts
@@ -1,63 +1,64 @@
-import { Injectable } from '@angular/core';
-import { ServicioVehiculoService } from './servicio-vehiculo.service';
-import { vehiculo } from './vehiculo.models';
-import { DataService } from './data.service';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+    import { Injectable } from '@angular/core';
+    import { ServicioVehiculoService } from './servicio-vehiculo.service';
+    import { vehiculo } from './vehiculo.models';
+    import { DataService } from './data.service';
+    import { BehaviorSubject, Observable, tap } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class vehiculosService {
+    @Injectable({
+      providedIn: 'root'
+    })
+    export class vehiculosService {
 
-  private vehiculosSubject = new BehaviorSubject<vehiculo[]>([]);  
-    vehiculos$ = this.vehiculosSubject.asObservable();
+      private vehiculosSubject = new BehaviorSubject<vehiculo[]>([]);  
+        vehiculos$ = this.vehiculosSubject.asObservable();
 
-  vehiculos: vehiculo[]=[];
+      vehiculos: vehiculo[]=[];
 
-  encontar_vehiculo(indice: any): vehiculo {
-    throw new Error('Method not implemented.');
-  }
-  eliminarVehiculo(vehiculoId: number) {
-    throw new Error('Method not implemented.');
-  }
-  vehiculo: vehiculo[] = [];
+      encontar_vehiculo(indice: any): vehiculo {
+        throw new Error('Method not implemented.');
+      }
+      eliminarVehiculo(vehiculoId: number) {
+        throw new Error('Method not implemented.');
+      }
+      vehiculo: vehiculo[] = [];
 
-  constructor(
-    private servicioMensaje: ServicioVehiculoService,
-    private dataService: DataService
-  ) {}
+      constructor(
+        private servicioMensaje: ServicioVehiculoService,
+        private dataService: DataService
+      ) {}
 
-  agregar_vehiculo_servicio(vehiculo: vehiculo): Observable<any> {
-    this.servicioMensaje.muestra_mensaje('Nombre Ingresado: ' + vehiculo.marca);
-    return this.dataService.agregarVehiculo(vehiculo).pipe(
-      tap((respuesta) => {
-        console.log('Vehículo agregado:', respuesta);
-        this.vehiculo.push(vehiculo);
-      })
-    );
-  }
+      agregar_vehiculo_servicio(vehiculo: vehiculo): Observable<any> {
+        this.servicioMensaje.muestra_mensaje('Nombre Ingresado: ' + vehiculo.marca);
+        return this.dataService.agregarVehiculo(vehiculo).pipe(
+          tap((respuesta) => {
+            console.log('Vehiculo agregado:', respuesta);
+            this.vehiculo.push(vehiculo);
+          })
+        );
+      }
 
-  eliminar_vehiculo_servicio(indice: number) {
-    const vehiculoId = "eliminar"; 
-    this.dataService.eliminarVehiculo(vehiculoId).subscribe(() => {
-      this.vehiculo.splice(indice, 1);
-      console.log(`Vehículo en índice ${indice} eliminado.`);
-    });
-  }
+      eliminar_vehiculo_servicio(indice: number) {
+        const vehiculoId = "eliminar"; 
+        this.dataService.eliminarVehiculo(vehiculoId).subscribe(() => {
+          this.vehiculo.splice(indice, 1);
+          console.log(`Vehiculo en indice ${indice} eliminado.`);
+        });
+      }
 
-  // Método para cargar los vehículos desde Firebase
-  cargarVehiculos() {  
-    this.dataService.obtenerVehiculos().subscribe((vehiculos) => {  
-      console.log('Vehículos desde Firebase:', vehiculos); // Agrega este console.log  
-      this.vehiculosSubject.next(vehiculos);  
-    });  
-  }
+      // metodo para cargar los vehículos desde Firebase
 
-  actualizar_vehiculo(indice: number, vehiculo: vehiculo) {
-    const vehiculoId = "actualizar"; 
-    this.dataService.actualizarVehiculo(vehiculoId, vehiculo).subscribe(() => {
-      console.log(`Vehículo en índice ${indice} actualizado en Firebase.`);
-      this.vehiculo[indice] = vehiculo;
-    });
-  }
-}
+      cargarVehiculos() {  
+        this.dataService.obtenerVehiculos().subscribe((vehiculos) => {  
+          console.log('Vehículos desde Firebase:', vehiculos); 
+          this.vehiculosSubject.next(vehiculos);  
+        });  
+      }
+
+      actualizar_vehiculo(indice: number, vehiculo: vehiculo) {
+        const vehiculoId = "actualizar"; 
+        this.dataService.actualizarVehiculo(vehiculoId, vehiculo).subscribe(() => {
+          console.log(`Vehiculo en indice ${indice} actualizado en Firebase.`);
+          this.vehiculo[indice] = vehiculo;
+        });
+      }
+    }
